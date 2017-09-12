f4b91e034b832efaa6f8936bed0efca028410796
Ethan <cook.ethan.j@gmail.com>
2017-09-11 22:59:49 -0400

    September 11th, 2017

--- Assets/Scripts/CampMenu.cs
+++ Assets/Scripts/CampMenu.cs
@@ -1,46 +1,117 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CampMenu : MonoBehaviour
 {
     public Text civText;
-    public int civValue;
+    public int civValue = 1;
+    public int civMax = 10;
 
     public Text foodText;
     public int foodValue;
+    public int foodMax = 50;
 
     public Text waterText;
     public int waterValue;
+    public int waterMax = 50;
+
+    public Text campLvlText;
+    public int campLvl = 1;
+
+    public Text suppliesText;
+    public int suppliesValue;
+    public int suppliesMax = 10;
 
     public GameObject MainCampMenu;
     public GameObject LootMenu;
+    public GameObject BuildMenu;
 
     public Text lootText;
 
-    private string[] scenarios = { "Food", "Water", "Citizens" };
+    public Text upgradeText;
+    public Button upgradeButton;
+
+    public Text noSuppliesText;
+
+    public Text lostCivText;
+
+    private string[] scenarios = { "Food", "Water", "Citizens", "Supplies" };
 
     void Start()
     {
         SetFood();
         SetWater();
         SetCivs();
-    }
+        SetCampLvl();
+        SetSupplies();
 
+        InvokeRepeating("FoodLoop", 1.0f, 5.0f);
+        InvokeRepeating("WaterLoop", 1.0f, 5.0f);
+    }
     void SetFood()
     {
-        foodText.text = "Food: " + foodValue.ToString();
+        if (foodValue >= foodMax)
+        {
+            foodValue = foodMax;
+        }
+
+        if (foodValue <= 0)
+        {
+            foodValue = 0;
+            StarveCiv();
+        }
+        foodText.text = "Food: " + foodValue.ToString() + "/" + foodMax.ToString();
     }
 
     void SetWater()
     {
-        waterText.text = "Water: " + waterValue.ToString();
+        if (waterValue >= waterMax)
+        {
+            waterValue = waterMax;
+        }
+
+        if (waterValue <= 0)
+        {
+            waterValue = 0;
+            StarveCiv();
+        }
+        waterText.text = "Water: " + waterValue.ToString() + "/" + waterMax.ToString();
     }
 
     void SetCivs()
     {
-        civText.text = "Citizens: " + civValue.ToString();
+        if (civValue >= civMax)
+        {
+            civValue = civMax;
+        }
+
+        if (civValue <= 0)
+        {
+            PlayerPrefs.SetInt("Food", foodValue);
+            PlayerPrefs.SetInt("Water", waterValue);
+            PlayerPrefs.SetInt("Supplies", suppliesValue);
+            PlayerPrefs.SetInt("Civilians", civValue);
+            PlayerPrefs.SetInt("CampLevel", campLvl);
+            SceneManager.LoadSceneAsync("End", LoadSceneMode.Single);
+        }
+        civText.text = "Citizens: " + civValue.ToString() + "/" + civMax.ToString();
+    }
+
+    void SetCampLvl()
+    {
+        campLvlText.text = "Camp Level: " + campLvl.ToString();
+    }
+
+    void SetSupplies()
+    {
+        if (suppliesValue >= suppliesMax)
+        {
+            suppliesValue = suppliesMax;
+        }
+        suppliesText.text = "Supplies: " + suppliesValue.ToString() + "/" + suppliesMax.ToString();
     }
 
     void GoLoot()
@@ -60,33 +131,164 @@ public class CampMenu : MonoBehaviour
             case 2:
                 LootCivilians();
                 break;
+            case 3:
+                LootSupplies();
+                break;
         }
     }
 
     void LootFood()
     {
-        lootText.text = "You found 1 food.";
-        foodValue = foodValue + 1;
+        int lootFound = Random.Range(0, 10);
+        lootText.text = "You found " + lootFound.ToString() + " food.";
+        foodValue = foodValue + lootFound;
         SetFood();
     }
 
     void LootWater()
     {
-        lootText.text = "You found 1 water.";
-        waterValue = waterValue + 1;
+        int lootFound = Random.Range(0, 10);
+        lootText.text = "You found " + lootFound.ToString() + " water.";
+        waterValue = waterValue + lootFound;
         SetWater();
     }
 
     void LootCivilians()
     {
-        lootText.text = "You found on 1 civilian";
-        civValue = civValue + 1;
+        int lootFound = Random.Range(0, 4);
+        lootText.text = "You found " + lootFound.ToString() +" civilians.";
+        civValue = civValue + lootFound;
         SetCivs();
     }
 
-    void BackMain()
+    void LootSupplies()
+    {
+        int lootFound = Random.Range(0, 10);
+        lootText.text = "You found " + lootFound.ToString() + " supplies.";
+        suppliesValue = suppliesValue + lootFound;
+        SetSupplies();
+    }
+
+    void BuildCamp()
+    {
+        MainCampMenu.SetActive(false);
+        BuildMenu.SetActive(true);
+
+        switch (campLvl)
+        {
+            case 1:
+                upgradeText.text = "Upgrade to level 2";
+                if (suppliesValue >= 10)
+                {
+                    upgradeButton.interactable = true;
+                    noSuppliesText.gameObject.SetActive(true);
+                    noSuppliesText.text = "Cost: 10";
+                }
+                else
+                {
+                    noSuppliesText.gameObject.SetActive(true);
+                    noSuppliesText.text = "You need 10 supplies for the next level.";
+                }
+                break;
+            case 2:
+                upgradeText.text = "Upgrade to level 3";
+                if (suppliesValue >= 20)
+                {
+                    upgradeButton.interactable = true;
+                    noSuppliesText.gameObject.SetActive(true);
+                    noSuppliesText.text = "Cost: 20";
+                }
+                else
+                {
+                    noSuppliesText.gameObject.SetActive(true);
+                    noSuppliesText.text = "You need 20 supplies for the next level.";
+                }
+                break;
+            case 3:
+                upgradeText.text = "Upgrade to level 4";
+                if (suppliesValue >= 30)
+                {
+                    upgradeButton.interactable = true;
+                    noSuppliesText.gameObject.SetActive(true);
+                    noSuppliesText.text = "Cost: 30";
+                }
+                else
+                {
+                    noSuppliesText.gameObject.SetActive(true);
+                    noSuppliesText.text = "You need 30 supplies for the next level.";
+                }
+                break;
+            case 4:
+                upgradeText.text = "You can't upgrade anymore";
+                noSuppliesText.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    void UpgradeCamp()
+    {
+        upgradeButton.interactable = false;
+        int suppliesUsed = campLvl * 10;
+        campLvl = campLvl + 1;
+        suppliesValue = suppliesValue - suppliesUsed;
+        foodMax = foodMax * 2;
+        waterMax = waterMax * 2;
+        suppliesMax = suppliesMax * 2;
+        civMax = civMax * 2;
+        SetFood();
+        SetWater();
+        SetCivs();
+        SetSupplies();
+        SetCampLvl();
+        BuildCamp();
+    }
+
+    void FoodLoop()
+    {
+        int foodDecrease = civValue * 2;
+        foodValue = foodValue - foodDecrease;
+        SetFood();
+    }
+
+    void WaterLoop()
+    {
+        int waterDecrease = civValue * 4;
+        waterValue = waterValue - waterDecrease;
+        SetWater();
+    }
+
+    void StarveCiv()
+    {
+        int starveChance = Random.Range(0, 4);
+        if (starveChance == 0 || starveChance == 4)
+        {
+            civValue = civValue - 1;
+            SetCivs();
+            lostCivText.gameObject.SetActive(true);
+            Invoke("notifyDeath", 5f);
+        };
+    }
+
+    void notifyDeath ()
+    {
+        lostCivText.gameObject.SetActive(false);
+    }
+
+    void BackLoot()
     {
         LootMenu.SetActive(false);
         MainCampMenu.SetActive(true);
+    }
+
+    void BackUpgrade()
+    {
+        BuildMenu.SetActive(false);
+        MainCampMenu.SetActive(true);
+
+        if (upgradeButton.interactable)
+        {
+            upgradeButton.interactable = false;
+            noSuppliesText.gameObject.SetActive(false);
+        }
     }
 }
---
+++ Assets/Scripts/EndMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EndMenu : MonoBehaviour {
+
+    public Text civText;
+    public Text foodText;
+    public Text waterText;
+    public Text campLvlText;
+    public Text suppliesText;
+
+    public int foodValue = 0;
+    public int waterValue = 0;
+    public int suppliesValue = 0;
+    public int civValue = 0;
+    public int campLvl = 0;
+
+    void Start()
+    {
+        
+        foodValue = PlayerPrefs.GetInt("Food");
+        waterValue = PlayerPrefs.GetInt("Water");
+        suppliesValue = PlayerPrefs.GetInt("Supplies");
+        civValue = PlayerPrefs.GetInt("Civilians");
+        campLvl = PlayerPrefs.GetInt("CampLevel");
+
+        foodText.text = "Food: " + foodValue;
+        waterText.text = "Water: " + waterValue;
+        suppliesText.text = "Supplies: " + suppliesValue;
+        civText.text = "Civilians: " + civValue;
+        campLvlText.text = "Camp Level: " + campLvl;
+    }
+
+    void BacktoMain()
+    {
+        SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
+    }
+}
